84d20937e8465a1baf3e962747d0a31933b65389
Henrique Broering Rohden <henrique.rohden@basis.com.br>
2020-03-03 13:18:23 -0300

    Add UserRepository

---
+++ src/main/java/br/com/henriquerohden/springauthapi/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.henriquerohden.springauthapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(@Param("username") String username);
+}
