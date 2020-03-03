6a95cf3947b62924b70a56cf932eb882a2111235
Henrique Broering Rohden <henrique.rohden@basis.com.br>
2020-03-03 18:29:29 -0300

    Block all requests by default

--- src/main/java/br/com/henriquerohden/springauthapi/SpringSecurityConfiguration.java
+++ src/main/java/br/com/henriquerohden/springauthapi/SpringSecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.com.henriquerohden.springauthapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,7 +25,9 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/auth").permitAll()
+            .anyRequest().denyAll()
         .and().csrf().disable()
         .headers().frameOptions().disable();
     }
