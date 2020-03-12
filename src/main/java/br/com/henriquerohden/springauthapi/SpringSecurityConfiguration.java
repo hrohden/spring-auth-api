ec5fdc0a5443e1fbe4f629439034f6a313f6ac84
Henrique Broering Rohden <henrique.rohden@basis.com.br>
2020-03-12 12:42:19 -0300

    Add bean AuthenticationManager

--- src/main/java/br/com/henriquerohden/springauthapi/SpringSecurityConfiguration.java
+++ src/main/java/br/com/henriquerohden/springauthapi/SpringSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -35,5 +36,10 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
